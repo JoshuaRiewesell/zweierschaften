b0efc63e4a4e410e6f89fdc2435ced8b0d185ae3
JoshuaRiewesell <40123349+JoshuaRiewesell@users.noreply.github.com>
2024-03-19 20:00:03 +0100

    alle 6 Spalten, grundlegende CSS

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <script setup lang="ts">
-  import Persons from '@/components/Persons.vue'
-  import { usePersonsStore } from '@/stores/persons'
+import Persons from '@/components/Persons.vue'
+import { usePersonsStore } from '@/stores/persons'
 
-  const patientsStore = usePersonsStore()
+const patientsStore = usePersonsStore()
 
-  patientsStore.loadFromLocalStorage()
+patientsStore.loadFromLocalStorage()
 </script>
 
 <template>
   <main>
-    <Persons />
+    <div class="grid">
+      <div class="column">
+        <h1>Zweierschaft</h1>
+        <form>
+          <button class="reset-btton" onclick="clearAll()">Reset</button>
+        </form>
+      </div>
+
+      <Persons />
+
+      <div class="column">
+        <h1>Filter</h1>
+        <div>
+          <button>Gebieten</button>
+          <button>Verbieten</button>
+        </div>
+      </div>
+
+      <div class="column">
+        <h1>Sets</h1>
+        <form class="sets-form">
+          <button type="submit" class="generate-button">Generate</button>
+        </form>
+        <ul class="set-items"></ul>
+      </div>
+
+      <div class="column">
+        <h1>Verwendet</h1>
+        <div>
+          <button>Export</button>
+        </div>
+        <ul class="usedSet-items"></ul>
+      </div>
+
+      <div class="column">
+        <h1>Ansicht</h1>
+        <div>
+          <button onclick="copyView()">Copy</button>
+        </div>
+        <ul class="pair-items" id="pair-items"></ul>
+      </div>
+    </div>
   </main>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/Persons.vue
+++ src/components/Persons.vue
@@ -1,15 +1,20 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div class="container">
-    <h1>PERSONEN</h1>
-    <form class="person-form" @submit.prevent="addPerson">
-      <input type="text" class="person-input" v-model="newPersonName" placeholder="Add a person...">
-      <button type="submit" class="add-button">Add</button><br>
-      <input type="file" id="csvFile" accept=".csv" />
+  <div class="column">
+    <h1>Personen</h1>
+    <form @submit.prevent="addPerson">
+      <input
+        type="text"
+        class="person-input"
+        v-model="newPersonName"
+        placeholder="Add a person..."
+      />
+      <button type="submit" class="add-button">Add</button><br />
     </form>
-    <ul class="person-items">
+    <!--<input type="file" id="csvFile" accept=".csv" />-->
+    <ul>
       <li v-for="(person, index) in persons" :key="index" class="item">
-        <input type="checkbox" class="checkbox">
+        <input type="checkbox" class="checkbox" />
         {{ person.name }}
         <button class="delete-button" @click="deletePerson(person)">X</button>
       </li>
@@ -24,7 +29,7 @@ const store = usePersonsStore()
 
 const persons = store.persons
 
-let newPersonName = '';
+let newPersonName = ''
 
 const addPerson = () => {
   if (newPersonName.trim() !== '') {
@@ -37,3 +42,18 @@ const deletePerson = (person: Person) => {
   store.removePerson(person.name)
 }
 </script>
+
+<style scoped>
+.person-input {
+  border: none;
+  outline: none;
+  border-radius: 5px;
+  padding: 10px;
+  font-size: 1rem;
+  width: 100%;
+}
+
+form {
+  display: flex;
+}
+</style>
